f1026823ef5c4083b38f918275c5f34b1ad529b5
mm <mm@gmail.com>
2020-09-22 11:08:11 +0200

    Adding Junit test

--- src/test/java/com/formation/ApplicationTests.java
+++ src/test/java/com/formation/ApplicationTests.java
@@ -1,16 +1,23 @@
 package com.formation;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.formation.service.Calculator;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testSum() {
+         assertEquals(5, calculator.sum(2, 3));
+    }
 
 }
